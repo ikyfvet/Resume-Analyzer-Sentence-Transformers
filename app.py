f40ff84305c3b615597cb342cb099e9240c268dd
ikyfvet <86878777+ikyfvet@users.noreply.github.com>
2025-05-16 17:55:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from resume_analyzer import ResumeAnalyzer
 import os
 
 def main():
-    st.set_page_config(page_title="Smart Resume Analyzer", layout="wide")
-    st.title("Smart Resume Analyzer")
+    st.set_page_config(page_title="Smart Resume Analyzerll", layout="wide")
+    st.title("Smart Resume Analyzerll")
 
     job_description = st.text_area("Enter the Job Description", height=200)
     st.write("Upload resumes as PDF files:")
